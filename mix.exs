5c19482ea9797e59df2158b0f412e7b0079f366a
sikanhe <sikanh@gmail.com>
2016-03-24 04:08:53 -0400

    added poison dep

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Algolia.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:hackney, github: "benoitc/hackney"}]
+    [{:hackney, github: "benoitc/hackney"},
+     {:poison, "~> 1.5"}]
   end
 end
