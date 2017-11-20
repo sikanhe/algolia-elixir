b1079269996f2510babdd8f2c4d8b9a9b75abd93
Sikan He <sikan@atriumlts.com>
2017-11-19 22:26:51 -0800

    0.6.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Algolia.Mixfile do
 
   def project do
     [app: :algolia,
-     version: "0.6.4",
+     version: "0.6.5",
      description: "Elixir implementation of Algolia Search API",
      elixir: "~> 1.2",
      package: package(),
